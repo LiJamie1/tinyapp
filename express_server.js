94f23c1997981b2220ca402be9cdb5a41247d714
Jamie Li <lijamie19828@gmail.com>
2021-11-09 00:58:54 +0000

    added app.post and added generateRandomString function, installed body parse, fixed misplaced include tag in urls_index and show

--- express_server.js
+++ express_server.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 8080; // default port 8080
 
+const generateRandomString = function() {
+  return Math.floor((1 + Math.random()) * 0x1000000).toString(16).substring(1);
+}
+
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -18,16 +24,25 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get('/urls/new', (req, res) => {
+  const longURL = req.body.longURL;
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+
 app.get('/urls/:shortURL', (req, res) => {
   const shortenedURL = req.params.shortURL;
   const templateVars = {shortURL: shortenedURL, longURL: urlDatabase[shortenedURL]};
   res.render('urls_show', templateVars);
 });
 
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
