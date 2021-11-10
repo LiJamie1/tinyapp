dca408de185510dfef63ee165b298cbbbb193b05
Jamie Li <lijamie19828@gmail.com>
2021-11-10 04:09:37 +0000

    installed cookie parser, added login button with text input, added app.post to make cookie from username

--- express_server.js
+++ express_server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const bodyParser = require('body-parser');
+const cookie = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 8080; // default port 8080
+
+app.use(cookie())
 
 const generateRandomString = function() {
   return Math.floor((1 + Math.random()) * 0x1000000).toString(16).substring(1);
@@ -16,16 +20,25 @@ const urlDatabase = {
 };
 
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.redirect("/urls")
 });
 
 app.get('/urls', (req, res) => {
-  const templateVars = {urls: urlDatabase};
+  const templateVars = {
+    username: req.cookies[username],
+    urls: urlDatabase
+  };
   res.render('urls_index', templateVars);
 });
 
 app.get('/urls/new', (req, res) => {
   res.render("urls_new");
+});
+
+app.post("/login", (req, res) => {
+  const username = req.body.username  
+  res.cookie('username', username)
+  res.redirect("/urls")
 });
 
 app.post("/urls", (req, res) => {
