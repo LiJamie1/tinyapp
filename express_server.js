00dd3f906570ea00db365eb7110803b3feab37b5
Jamie Li <lijamie19828@gmail.com>
2021-11-11 00:10:15 +0000

    made new page for registration, wrote app.post to add a new object with fields id, email and password

--- express_server.js
+++ express_server.js
@@ -38,6 +38,13 @@ app.get('/urls/new', (req, res) => {
   res.render("urls_new", templateVars);
 });
 
+app.get("/register", (req, res) => {
+  const templateVars = {
+    username: req.cookies.username,
+  };
+  res.render('urls_register', templateVars);
+});
+
 app.post("/login", (req, res) => {
   const username = req.body.username  
   res.cookie('username', username)
@@ -67,7 +74,39 @@ app.post("/urls/:shortURL", (req, res) => {
 app.post("/logout", (req, res) => {
   res.clearCookie("username")
   res.redirect("/urls")
+});
+
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "qwertyuiop"
+  },
+  "userRandomID2": {
+    id: "userRandomID2",
+    email: "user2@example.com",
+    password: "asdfghjkl"
+  }
+}
+
+app.post("/register", (req, res) => {
+  const id = generateRandomString();
+  const password = req.body.password
+  const email = req.body.email
+  //add new object to users
+  //object should include id, email and password
+  //id is randomly generated
+  users[id] = {
+    id,
+    email,
+    password
+  };
+  //set a user_id cookie containing newly generated id
+  res.cookie(id, users[id])
+  res.redirect("/urls")
 })
+
+
 
 app.get('/urls/:shortURL', (req, res) => {
   const shortURL = req.params.shortURL;
