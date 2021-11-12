8a3d644fed43489664ca51e314cd744d0b09cb4a
Jamie Li <lijamie19828@gmail.com>
2021-11-12 02:01:04 +0000

    wrote one test for findUserByEmail, test passes

--- test/helperTests.js
+++ test/helperTests.js
@@ -2,6 +2,7 @@ const chai = require('chai');
 const assert = chai.assert;
 const {generateRandomString, findUserByEmail, userUrls} = require('../helpers');
 
+
 describe('#generateRandomString', function() {
   it('if called twice it should return as they are not equal', function() {
     const randomStringOne = generateRandomString();
@@ -11,5 +12,30 @@ describe('#generateRandomString', function() {
   it('should return a 6 character string when called', function () {
     const randomStringLength = generateRandomString().length;
     assert.strictEqual(randomStringLength, 6);
+  });
+});
+
+describe('#findUserByEmail', function() {
+  it('should return an object containing information on the current user', function() {
+    const targetUser = {
+      id: 1111,
+      email: 'j@j.ca',
+      password: 123
+    };
+    const targetUserEmail = 'j@j.ca';
+    const users = {
+      user1: {
+        id: 1111,
+        email: 'j@j.ca',
+        password: 123
+      },
+      user2: {
+        id: 2222,
+        email: 'k@k.ca',
+        password: 234
+      }
+    };
+    const result = findUserByEmail(targetUserEmail, users);
+    assert.deepEqual(result, targetUser);
   });
 });
